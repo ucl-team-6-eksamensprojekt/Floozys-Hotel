991706c0536a7762ea1d1e63c13ec147ff532df3
Nickolai-Nygaard <nygaardnickolai@gmail.com>
2025-11-15 16:01:24 +0100

    slight changes to menu

---
+++ Floozys Hotel/ViewModels/MainViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Floozys_Hotel.Commands;
+using Floozys_Hotel.Core;
+
+namespace Floozys_Hotel.ViewModels
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand BookingOverviewViewCommand { get; set; }
+        public RelayCommand GuestOverviewViewCommand { get; set; }
+        public RelayCommand RoomOverviewViewCommand { get; set; }
+        public RelayCommand SalesOverviewViewCommand { get; set; }
+        public RelayCommand GuestPolicyViewCommand { get; set; }
+
+
+
+        public BookingOverviewViewModel BookingVM { get; set; }
+        public GuestOverviewViewModel GuestVM { get; set; }
+        public RoomOverviewViewModel RoomVM { get; set; }
+        public SalesOverviewViewModel SalesVM { get; set; }
+        public GuestPolicyViewModel PolicyVM { get; set; }
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            BookingVM = new BookingOverviewViewModel();
+            GuestVM = new GuestOverviewViewModel();
+            RoomVM = new RoomOverviewViewModel();
+            SalesVM = new SalesOverviewViewModel();
+            PolicyVM = new GuestPolicyViewModel();
+            CurrentView = BookingVM;
+
+            BookingOverviewViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = BookingVM;
+            });
+
+            GuestOverviewViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = GuestVM;
+            });
+
+            RoomOverviewViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = RoomVM;
+            });
+
+            SalesOverviewViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = SalesVM;
+            });
+
+            GuestPolicyViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = PolicyVM;
+            });
+        }
+    }
+}

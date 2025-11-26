720a5a8c973cd9afa16c93723658027e51cdb89c
codemikemike <mikr71394@edu.ucl.dk>
2025-11-26 09:24:27 +0100

    add NewBookingViewModel, NewBookingView and XAML code (frontend UI) for NewBooking view.

---
+++ Floozys Hotel/ViewModels/NewBookingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Floozys_Hotel.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Floozys_Hotel.ViewModels
+{
+    class NewBookingViewModel : ObservableObject
+    {
+
+
+    }
+}

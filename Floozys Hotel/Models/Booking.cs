557b2cbdce83ee7ec8bdfc5c9bccf7a5bfd7cee3
codemikemike <mikr71394@edu.ucl.dk>
2025-11-27 23:47:33 +0100

    Add Booking model with custom date validation attribute

--- Floozys Hotel/Models/Booking.cs
+++ Floozys Hotel/Models/Booking.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.ComponentModel.DataAnnotations;
+using Floozys_Hotel.Validation;
 
 namespace Floozys_Hotel.Models
 {
     class Booking
     {
+        // Primary Key (IDENTITY in SQL)
+        public int BookingID { get; init; }
+
+        // Start Date (DATE in SQL)
+        [Required(ErrorMessage = "Start date is required")]
+        public DateTime StartDate { get; set; }
+
+        // End Date (DATE in SQL) - Must be AFTER StartDate
+        [Required(ErrorMessage = "End date is required")]
+        [DateGreaterThan(nameof(StartDate), ErrorMessage = "End date must be after start date")]
+        public DateTime EndDate { get; set; }
+
+        // Check-In Time (DATETIME2 in SQL)
+        [Required(ErrorMessage = "Check-in time is required")]
+        public DateTime CheckInTime { get; set; }
+
+        // Check-Out Time (DATETIME2 in SQL) - Must be AFTER CheckInTime
+        [Required(ErrorMessage = "Check-out time is required")]
+        [DateGreaterThan(nameof(CheckInTime), ErrorMessage = "Check-out time must be after check-in time")]
+        public DateTime CheckOutTime { get; set; }
+
+        // Foreign Keys
+        [Required(ErrorMessage = "Room is required")]
+        public int RoomID { get; set; }
+
+        [Required(ErrorMessage = "Guest is required")]
+        public int GuestID { get; set; }
     }
 }

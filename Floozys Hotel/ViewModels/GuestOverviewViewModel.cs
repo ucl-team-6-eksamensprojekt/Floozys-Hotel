a74ca7e2252ac61091f550e87b823a0afd2fd813
Anna Lundin <aolu71339@edu.ucl.dk>
2025-11-29 18:28:37 +0100

    Create columns, rows, border and button #120

--- Floozys Hotel/ViewModels/GuestOverviewViewModel.cs
+++ Floozys Hotel/ViewModels/GuestOverviewViewModel.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Floozys_Hotel.Commands;
+using Floozys_Hotel.Core;
 
 namespace Floozys_Hotel.ViewModels
 {
-    class GuestOverviewViewModel
+    public class GuestOverviewViewModel : ObservableObject
     {
+        public RelayCommand NewGuestCommand { get; set; }
+
+        public GuestOverviewViewModel() 
+        {
+            NewGuestCommand = new RelayCommand
+                (n => OpenNewGuest());
+        }
+
+        private void OpenNewGuest()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

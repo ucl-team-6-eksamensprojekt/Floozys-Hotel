489e262934839e37b0aa07800a3a801e82dbe54d
Nickolai-Nygaard <nygaardnickolai@gmail.com>
2025-12-08 14:54:30 +0100

    room and roomsform

--- Floozys Hotel/ViewModels/RoomOverviewViewModel.cs
+++ Floozys Hotel/ViewModels/RoomOverviewViewModel.cs
@@ -1,7 +1,11 @@
-﻿using Floozys_Hotel.Core;
+﻿using Floozys_Hotel.Commands;
+using Floozys_Hotel.Core;
 using Floozys_Hotel.Models;
 using Floozys_Hotel.Repositories;
+using Floozys_Hotel.ViewModels.FormsViewModel;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+
 
 namespace Floozys_Hotel.ViewModels
 {
@@ -11,11 +15,55 @@ namespace Floozys_Hotel.ViewModels
 
         public ObservableCollection<Room> Rooms { get; set; }
 
+
+
+        private bool _isRoomFormOpen;
+        public bool IsRoomFormOpen
+        {
+            get => _isRoomFormOpen;
+            set
+            {
+                if (_isRoomFormOpen != value)
+                {
+                    _isRoomFormOpen = value;
+                    OnPropertyChanged();
+                }
+            
+            }
+        }
+
+        private object _currentSideContent;
+        public object CurrentSideContent
+        {
+            get => _currentSideContent;
+            set 
+            {
+                if (_currentSideContent != value)
+                {
+                    _currentSideContent = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+
+        public ICommand OpenCreateRoomCommand { get; }
+        public ICommand OpenEditRoomCommand { get; }
+        public ICommand DeleteRoomCommand { get; }
+
+
         public RoomOverviewViewModel()
         {
             _roomRepo = new RoomRepo();
             Rooms = new ObservableCollection<Room>();
             LoadRooms();
+
+            OpenCreateRoomCommand = new RelayCommand(_ => OpenCreateRoom());
+            OpenEditRoomCommand = new RelayCommand(r => OpenEditRoom(r as Room));
+            DeleteRoomCommand = new RelayCommand(r => DeleteRoom(r as Room));
+
         }
 
         private void LoadRooms()
@@ -26,6 +74,52 @@ namespace Floozys_Hotel.ViewModels
             {
                 Rooms.Add(room);
             }
+        }
+
+        private void OpenCreateRoom()
+        {
+            CurrentSideContent = new RoomFormViewModel(
+                _roomRepo,
+                onSaved: OnRoomSaved,
+                onCancel: ClosePanel,
+                existingRoom: null);
+
+            IsRoomFormOpen = true;
+        }
+
+        private void OpenEditRoom(Room room)
+        {
+            if (room == null) return;
+
+            CurrentSideContent = new RoomFormViewModel(
+                _roomRepo,
+                onSaved: OnRoomSaved,
+                onCancel: ClosePanel,
+                existingRoom: room);
+
+            IsRoomFormOpen = true;
+        }
+
+        
+
+        private void DeleteRoom(Room room)
+        {
+            if (room == null) return;
+
+            _roomRepo.DeleteRoom(room.RoomId);
+            Rooms.Remove(room);
+        }
+
+        private void OnRoomSaved()
+        {
+            LoadRooms();
+            ClosePanel();
+        }
+
+        private void ClosePanel()
+        {
+            IsRoomFormOpen = false;
+            CurrentSideContent = null;
         }
     }
 }
